0a33d9018e4cf1c0cdd5f5382ac1197e099b45ba
Marko Vitković <mavitkov@vsite.hr>
2020-02-12 16:22:33 +0100

    Add project files.

---
+++ miha/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace miha.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
