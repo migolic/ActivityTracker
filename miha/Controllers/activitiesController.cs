361f46cd8bb82da28c0fbac4c50b84f590e4297c
migolic <migolic@vsite.hr>
2020-02-20 11:36:44 +0100

    resolve excess comment

--- miha/Controllers/activitiesController.cs
+++ miha/Controllers/activitiesController.cs
@@ -84,7 +84,6 @@ namespace miha.Controllers
         // POST: activities/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //changed some things
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Ime,Tezina,Visina,Puls,Spavanje")] activity activity)
